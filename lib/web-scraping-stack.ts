e34a1cd9f5b233e02d0d1142ec9f1146841fae1f
ToanG1 <Toandinh01675@gmail.com>
2025-01-25 19:31:20 +0700

    fetch with puppeteer

--- lib/web-scraping-stack.ts
+++ lib/web-scraping-stack.ts
@@ -8,8 +8,8 @@ export class WebScrapingStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const playwrightLayer = new lambda.LayerVersion(this, "PlaywrightLayer", {
-      code: lambda.Code.fromAsset(path.join(__dirname, "../playwright-layer")),
+    const puppeteerLayer = new lambda.LayerVersion(this, "PuppeteerLayer", {
+      code: lambda.Code.fromAsset(path.join(__dirname, "../puppeteer-layer")),
       compatibleRuntimes: [lambda.Runtime.NODEJS_18_X],
     });
 
@@ -20,8 +20,8 @@ export class WebScrapingStack extends cdk.Stack {
         runtime: lambda.Runtime.NODEJS_18_X,
         code: lambda.Code.fromAsset(path.join(__dirname, "../lambda")),
         handler: "scroll-scraping.handler",
-        layers: [playwrightLayer],
-        memorySize: 1024,
+        layers: [puppeteerLayer],
+        memorySize: 2048,
         timeout: cdk.Duration.seconds(45),
       }
     );
