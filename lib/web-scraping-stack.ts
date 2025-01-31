2d06eb242686d2ffb8bd68405b1c21a65b50c0bf
ToanG1 <Toandinh01675@gmail.com>
2025-01-31 20:05:59 +0700

    add auth layer

--- lib/web-scraping-stack.ts
+++ lib/web-scraping-stack.ts
@@ -3,10 +3,33 @@ import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as path from "path";
+import * as secretsmanager from "aws-cdk-lib/aws-secretsmanager";
 
 export class WebScrapingStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const authorizerLambda = new lambda.Function(this, "AuthorizerLambda", {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: "index.handler",
+      code: lambda.Code.fromAsset(path.join(__dirname, "../lambda/auth")),
+    });
+
+    const authorizer = new apigateway.TokenAuthorizer(this, "MyAuthorizer", {
+      handler: authorizerLambda,
+    });
+
+    const secret = new secretsmanager.Secret(this, "AuthSecret", {
+      secretName: "WebScrapingAuthToken",
+      generateSecretString: {
+        secretStringTemplate: JSON.stringify({}),
+        generateStringKey: "token",
+        passwordLength: 32,
+        excludeCharacters: '"@/\\',
+      },
+    });
+
+    secret.grantRead(authorizerLambda);
 
     const puppeteerLayer = new lambda.LayerVersion(this, "PuppeteerLayer", {
       code: lambda.Code.fromAsset(path.join(__dirname, "../puppeteer-layer")),
@@ -18,8 +41,10 @@ export class WebScrapingStack extends cdk.Stack {
       "ScollScrapingLambda",
       {
         runtime: lambda.Runtime.NODEJS_18_X,
-        code: lambda.Code.fromAsset(path.join(__dirname, "../lambda")),
-        handler: "scroll-scraping.handler",
+        code: lambda.Code.fromAsset(
+          path.join(__dirname, "../lambda/scroll-scraping")
+        ),
+        handler: "index.handler",
         layers: [puppeteerLayer],
         memorySize: 2048,
         timeout: cdk.Duration.seconds(45),
@@ -35,6 +60,9 @@ export class WebScrapingStack extends cdk.Stack {
     );
 
     const scrollScrapingResource = api.root.addResource("scroll-scraping");
-    scrollScrapingResource.addMethod("GET", scrollScrapingIntegration);
+    scrollScrapingResource.addMethod("GET", scrollScrapingIntegration, {
+      authorizationType: apigateway.AuthorizationType.CUSTOM,
+      authorizer,
+    });
   }
 }
